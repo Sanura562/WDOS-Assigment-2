e37444437c2469d1a61f42f9199112e3edaf71fc
Sanura562 <apex13088@gmail.com>
2024-04-04 20:08:53 +0530

    #

--- wildlifeintro.js
+++ wildlifeintro.js
@@ -1,6 +1,6 @@
 async function fetchData() {
   try {
-    const response = await fetch("index.json"); // Replace 'data.json' with the path to your JSON file
+    const response = await fetch("wildlifeintro.json"); // Replace 'data.json' with the path to your JSON file
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
@@ -18,7 +18,7 @@ function storeDataLocally(data) {
   try {
     // Convert the data to a JSON string and store it in local storage
     const jsonData = JSON.stringify(data);
-    localStorage.setItem("indexData", jsonData); // 'websiteData' is the key to access the data in local storage
+    localStorage.setItem("wildlifeintroData", jsonData); // 'websiteData' is the key to access the data in local storage
     console.log("Data stored locally:", data);
   } catch (error) {
     console.error("Error storing data in local storage:", error);
@@ -36,7 +36,7 @@ fetchData()
   });
 -document.addEventListener("DOMContentLoaded", function () {
   // Fetch data from JSON file
-  fetch("index.json")
+  fetch("wildlifeintro.json")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -44,28 +44,29 @@ fetchData()
       return response.json();
     })
     .then((data) => {
-      document.getElementById("hero-title").innerHTML = data.hero.title;
-      document.getElementById("about-title").innerHTML = data.about.title;
-      document.getElementById("about-intro").innerHTML = data.about.intro;
-      document.getElementById("parks-title").innerHTML = data.topParks.title;
-      document.getElementById("facts-title").innerHTML = data.facts.title;
+      document.getElementById("sectionHeading").innerHTML =
+        data.section2.locations.about_location.section_heading;
+      document.getElementById("locationIntro").innerHTML =
+        data.section2.locations.about_location.location_intro;
 
-      ///loop through the parks array and create a list item for each park
-      data.topParks.parks.forEach((parkName) => {
-        const parkList = document.getElementById("park-lists");
-        const listItem = document.createElement("li");
+      document.getElementById("items_heading1").innerHTML =
+        data.flex_container.flex_item1.items_heading;
 
-        listItem.textContent = parkName;
-        parkList.appendChild(listItem);
-      });
+      //   data.topParks.parks.forEach((parkName) => {
+      //     const parkList = document.getElementById("park-lists");
+      //     const listItem = document.createElement("li");
 
-      data.topParks.parks.forEach((factName) => {
-        const factList = document.getElementById("facts-lists");
-        const listItem = document.createElement("li");
+      //     listItem.textContent = parkName;
+      //     parkList.appendChild(listItem);
+      //   });
 
-        listItem.textContent = factName;
-        factList.appendChild(listItem);
-      });
+      //   data.topParks.parks.forEach((factName) => {
+      //     const factList = document.getElementById("facts-lists");
+      //     const listItem = document.createElement("li");
+
+      //     listItem.textContent = factName;
+      //     factList.appendChild(listItem);
+      //   });
     })
     .catch((error) => {
       console.error("There was a problem with the fetch operation:", error);
