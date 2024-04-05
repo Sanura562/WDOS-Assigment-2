2d4208a644323f9e1a847f894f5ca9c4b977b7d9
Sanura562 <apex13088@gmail.com>
2024-04-05 16:50:52 +0530

    done until last page

--- wilpattu.js
+++ wilpattu.js
@@ -14,8 +14,33 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log(data);
       document.getElementById("wilpattu_heading").innerHTML =
         data.body.heading.wilpattu_heading;
-      document.getElementById("wilpattu_heading").innerHTML =
-        data.body.heading.wilpattu_heading;
+      document.getElementById("about_heading").innerHTML =
+        data.body.about.about_us;
+      document.getElementById("wilpattu_intro").innerHTML =
+        data.body.about.wilpattu_intro;
+      ///wilpattu images
+      const wl = data.body.wilpattu_images.Wl.src;
+      document.getElementById("wl").src = `./${wl}`;
+      ///wilpattu images
+      const he = data.body.wilpattu_images.HE.src;
+      document.getElementById("he").src = `./${he}`;
+      ///location
+      //
+      function renderIframeFromJSON(data) {
+        const iframeURL = data.body.location.iframe.src;
+        const iframeWidth = data.body.location.iframe.width;
+        const iframeHeight = data.body.location.iframe.height;
+
+        const iframe = document.createElement("iframe");
+
+        iframe.src = iframeURL;
+        iframe.width = iframeWidth;
+        iframe.height = iframeHeight;
+        iframe.style.padding = "0";
+
+        document.getElementById("wilpattu_loc").appendChild(iframe);
+      }
+      renderIframeFromJSON(data);
     })
     .catch((error) => {
       console.error("There was a problem with the fetch operation:", error);
---
+++ yala.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Fetch data from JSON file
+  fetch("yala.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((content) => {
+      localStorage.setItem("Yala", JSON.stringify(content));
+
+      const data = JSON.parse(localStorage.getItem("Yala"));
+      console.log(data);
+      ///yala
+      document.getElementById("yala_heading").innerHTML =
+        data.header.heading.title;
+      document.getElementById("about_heading").innerHTML = data.about.title;
+      document.getElementById("about_para").innerHTML = data.about.intro;
+      ///image
+      const yala_image = data.about.yala_image1.src;
+      document.getElementById("Yi").src = `./${yala_image}`;
+
+      document.getElementById("terrain_title").innerHTML = data.heading.title;
+      document.getElementById("terrain_paragraph").innerHTML = data.intro;
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+});
